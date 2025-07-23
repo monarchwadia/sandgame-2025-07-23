03d20e7bc25c393523f31a11a67c146fefb10003
Monarch Wadia <monarchwadia@gmail.com>
2025-07-23 15:03:50 -0400

    gridutils

---
+++ src/gridUtils.ts
@@ -0,0 +1,25 @@
+// src/gridUtils.ts
+// Utility functions for grid position calculations
+
+export function getIndex(x: number, y: number, width: number): number {
+  return y * width + x;
+}
+
+export function getBelow(x: number, y: number, width: number): number {
+  return getIndex(x, y + 1, width);
+}
+
+export function getLeft(x: number, y: number, width: number): number {
+  return getIndex(x - 1, y + 1, width);
+}
+
+export function getRight(x: number, y: number, width: number): number {
+  return getIndex(x + 1, y + 1, width);
+}
+
+export function getXY(index: number, width: number): { x: number, y: number } {
+  return {
+    x: index % width,
+    y: Math.floor(index / width)
+  };
+}
