bfb3966d7c45c1ce683c1c1d488ff606a807af00
Monarch Wadia <monarchwadia@gmail.com>
2025-07-23 15:36:44 -0400

    water moves left/right

--- src/particles/water.particle.ts
+++ src/particles/water.particle.ts
@@ -14,26 +14,26 @@ export const waterParticle: ParticleType = {
                 grid[below] = 2; // 2 = water
                 return;
             }
-            // Try to flow left or right
-            const left = getLeft(x, y, width);
-            const right = getRight(x, y, width);
-            const canLeft = x > 0 && grid[left] === 0;
-            const canRight = x < width - 1 && grid[right] === 0;
-            if (canLeft && canRight) {
-                if (Math.random() < 0.5) {
-                    grid[i] = 0;
-                    grid[left] = 2;
-                } else {
-                    grid[i] = 0;
-                    grid[right] = 2;
-                }
-            } else if (canLeft) {
+        }
+        // Try to move left or right if not moving down
+        const left = getIndex(x - 1, y, width);
+        const right = getIndex(x + 1, y, width);
+        const canLeft = x > 0 && grid[left] === 0;
+        const canRight = x < width - 1 && grid[right] === 0;
+        if (canLeft && canRight) {
+            if (Math.random() < 0.5) {
                 grid[i] = 0;
                 grid[left] = 2;
-            } else if (canRight) {
+            } else {
                 grid[i] = 0;
                 grid[right] = 2;
             }
+        } else if (canLeft) {
+            grid[i] = 0;
+            grid[left] = 2;
+        } else if (canRight) {
+            grid[i] = 0;
+            grid[right] = 2;
         }
     }
 };
