81fa0451fa030121ab62b8dd0c1b348093ee187d
Monarch Wadia <monarchwadia@gmail.com>
2025-07-31 00:36:37 -0400

    60fps

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@
 
 export const GRID_WIDTH = 120;
 export const GRID_HEIGHT = 120;
-export const FPS = 2000; // Frames per second for the game loop
+export const FPS = 60; // Frames per second for the game loop
 export const CELL_WIDTH = window.innerWidth / GRID_WIDTH;
 export const CELL_HEIGHT = window.innerHeight / GRID_HEIGHT;
 export const HOUR_INDEXES = {
