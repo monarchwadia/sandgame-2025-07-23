cbbe44eff386b683e6fd3030a1ccf3fc6f333b74
Monarch Wadia <monarchwadia@gmail.com>
2025-08-01 14:23:22 -0400

    removed gridutils

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,9 @@
 // src/constants.ts
 // Shared constants for the sandgame project
 
-export const GRID_WIDTH = 200;
-export const GRID_HEIGHT = 200;
-export const FPS = 1800; // Frames per second for the game loop
+export const GRID_WIDTH = 400;
+export const GRID_HEIGHT = 400;
+export const FPS = 60; // Frames per second for the game loop
 export const TICKS_TO_RENDER_RATIO = 2; // How many simulation ticks per render frame. This is an optimization to reduce rendering load.
 export const CELL_WIDTH = window.innerWidth / GRID_WIDTH;
 export const CELL_HEIGHT = window.innerHeight / GRID_HEIGHT;
